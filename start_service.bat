63d2c22fbb981b58de0f3dfc055c14bad277873a
xzaliman <83049855+xzaliman@users.noreply.github.com>
2021-05-15 21:27:39 +0700

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop RikaRobot
-net start RikaRobot
+net stop xz5pdbot
+net start xz5pdbot
